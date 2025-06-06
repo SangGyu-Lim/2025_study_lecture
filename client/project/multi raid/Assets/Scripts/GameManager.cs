61eec33fe023a119c6396f0b437c5c15a4fcb978
limsg <kilsoo1234@naver.com>
2025-06-06 17:46:34 +0900

    2week

--- client/project/multi raid/Assets/Scripts/GameManager.cs
+++ client/project/multi raid/Assets/Scripts/GameManager.cs
@@ -1,25 +1,20 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
     public Transform canvas;
 
+    protected override void Awake()
+    {
+        base.Awake();  // 싱글톤 초기화
+
+        Debug.Log("GameManager init");
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InitBtn();
-    }
-
-    void InitBtn()
-    {
-        canvas.Find("Login/LoginBtn").GetComponent<Button>().onClick.AddListener(OnClickLogin);
-        canvas.Find("Login/JoinBtn").GetComponent<Button>().onClick.AddListener(OnClickJoinPage);
-
-        canvas.Find("Join/BackBtn").GetComponent<Button>().onClick.AddListener(OnClickLoginPage);
-        canvas.Find("Join/JoinBtn").GetComponent<Button>().onClick.AddListener(OnClickJoin);
+        
     }
 
     // Update is called once per frame
@@ -27,49 +22,4 @@ public class GameManager : MonoBehaviour
     {
         
     }
-
-    void OnClickLogin()
-    {
-        string id = canvas.Find("Login/ID").GetComponent<InputField>().text;
-        string password = canvas.Find("Login/Password").GetComponent<InputField>().text;
-
-        Debug.Log("id : " + id + " pwd : " + password);
-
-        // todo 로그인 연결 - 임시로 씬전환
-        SceneManager.LoadScene("GameScene");
-        //NetworkManager.Instance.SendServer(API_TYPE.login, id, password);
-    }
-
-    void OnClickJoinPage()
-    {
-        canvas.Find("Login").gameObject.SetActive(false);
-        canvas.Find("Join").gameObject.SetActive(true);
-
-        canvas.Find("Join/ID").GetComponent<InputField>().text = "";
-        canvas.Find("Join/Password").GetComponent<InputField>().text = "";
-
-    }
-
-    void OnClickLoginPage()
-    {
-        canvas.Find("Login").gameObject.SetActive(true);
-        canvas.Find("Join").gameObject.SetActive(false);
-
-        canvas.Find("Login/ID").GetComponent<InputField>().text = "";
-        canvas.Find("Login/Password").GetComponent<InputField>().text = "";
-
-    }
-
-    void OnClickJoin()
-    {
-        string id = canvas.Find("Join/ID").GetComponent<InputField>().text;
-        string password = canvas.Find("Join/Password").GetComponent<InputField>().text;
-
-        Debug.Log("id : " + id + " pwd : " + password);
-
-        // todo 회원가입 연결 + 회원가입 이후 패킷 처리
-        //NetworkManager.Instance.SendServer(API_TYPE.join, id, password);
-    }
-
-
 }
---
+++ client/project/multi raid/Assets/Scripts/LoginManager.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    public Transform canvas;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InitBtn();
+    }
+
+    void InitBtn()
+    {
+        canvas.Find("Login/LoginBtn").GetComponent<Button>().onClick.AddListener(OnClickLogin);
+        canvas.Find("Login/JoinBtn").GetComponent<Button>().onClick.AddListener(OnClickJoinPage);
+
+        canvas.Find("Join/BackBtn").GetComponent<Button>().onClick.AddListener(OnClickLoginPage);
+        canvas.Find("Join/JoinBtn").GetComponent<Button>().onClick.AddListener(OnClickJoin);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClickLogin()
+    {
+        string id = canvas.Find("Login/ID").GetComponent<InputField>().text;
+        string password = canvas.Find("Login/Password").GetComponent<InputField>().text;
+
+        Debug.Log("id : " + id + " pwd : " + password);
+
+        // todo 로그인 연결 - 임시로 씬전환
+        SceneManager.LoadScene("GameScene");
+        //NetworkManager.Instance.SendServer(API_TYPE.login, id, password);
+    }
+
+    void OnClickJoinPage()
+    {
+        canvas.Find("Login").gameObject.SetActive(false);
+        canvas.Find("Join").gameObject.SetActive(true);
+
+        canvas.Find("Join/ID").GetComponent<InputField>().text = "";
+        canvas.Find("Join/Password").GetComponent<InputField>().text = "";
+
+    }
+
+    void OnClickLoginPage()
+    {
+        canvas.Find("Login").gameObject.SetActive(true);
+        canvas.Find("Join").gameObject.SetActive(false);
+
+        canvas.Find("Login/ID").GetComponent<InputField>().text = "";
+        canvas.Find("Login/Password").GetComponent<InputField>().text = "";
+
+    }
+
+    void OnClickJoin()
+    {
+        string id = canvas.Find("Join/ID").GetComponent<InputField>().text;
+        string password = canvas.Find("Join/Password").GetComponent<InputField>().text;
+
+        Debug.Log("id : " + id + " pwd : " + password);
+
+        // todo 회원가입 연결 + 회원가입 이후 패킷 처리
+        //NetworkManager.Instance.SendServer(API_TYPE.join, id, password);
+    }
+
+
+}
